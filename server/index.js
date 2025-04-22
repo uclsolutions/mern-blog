4a8c50c1c75c2dd041b5d0752ff1f790979f6c4d
shariful3github <shariful3kabir@gmail.com>
2025-04-22 20:10:33 +1200

    COnnect to Database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
 const app =express()
 
+mongoose.connect('mongodb://localhost:27017/mern-blog').then(()=>console.log('Mongo DB is connected')).catch(err=>{console.log(err)})
 app.listen(3000,()=>{
     console.log(`Server is listenig on port 3000`)
 })
